522e96fd59180ddf3732f178830428d5cccfe2b2
Arianna <Arianna@LAPTOP-LPB65445>
2019-03-18 13:37:07 +0100

    Implementato toString()

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,20 @@
 package it.polito.tdp.libretto;
 
-public class Libretto {
+import java.time.LocalDate;
+import java.util.*;
 
+public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti=new ArrayList<Voto>();
+	}
+	
+	//Aggiungere un nuovo voto al libretto
+	//@param v  il {@link Voto} da aggiungere
+	
+	public void add(Voto v) {
+		voti.add(v);
+	}
 }
---
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -0,0 +1,22 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;
+
+public class TestLibretto {
+	
+	public static void main(String[] args) {
+		
+		Libretto libr=new Libretto();
+		libr.add(new Voto(19,"Analisi I", LocalDate.of(2017, 1, 15)));
+		libr.add(new Voto(20,"Analisi II", LocalDate.of(2018, 2, 3)));
+		libr.add(new Voto(24,"Fisica I", LocalDate.of(2017, 6, 27)));
+		libr.add(new Voto(18,"Fisica II", LocalDate.of(2018, 1, 18)));
+		libr.add(new Voto(23,"Algebra Lineare e Geometria", LocalDate.of(2017, 7, 1)));
+		libr.add(new Voto(25,"Economia Aziendale", LocalDate.of(2018, 1, 28)));
+		libr.add(new Voto(25,"Ricerca Operativa", LocalDate.of(2018, 1, 26)));
+		libr.add(new Voto(24,"Diritto Privato", LocalDate.of(2019, 1, 23)));
+		libr.add(new Voto(30,"Programmazione a Oggetti", LocalDate.of(2018, 6, 25)));
+		libr.add(new Voto(27,"Sistemi di produzione", LocalDate.of(2018, 7, 3)));
+	}
+
+}
